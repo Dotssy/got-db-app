0c0f02e25be6f8e31c23e570a4c33ceca8d5fd9e
Bohdan Zozulya <zozulya.bohdan@gmail.com>
2021-06-28 16:18:11 +0300

    First commit

---
+++ src/service/got-service.js
@@ -0,0 +1,23 @@
+export default class GotService {
+	constructor() {
+		this._apiBase = 'https://www.anapioficeandfire.com/api';
+	}
+
+	async getResourse(url) {
+		const res = await fetch(`${this._apiBase}${url}`);
+
+		if (!res.ok) {
+			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+		}
+
+		return await res.json();
+	}
+
+	getAllChars() {
+		return this.getResourse(`/characters?page=5&pageSize=10`);
+	}
+
+	getChar(id) {
+		return this.getResourse(`/characters/${id}`);
+	}
+}
